46a69e48d13d6207bd2faeca7d46bdfa53c030b9
Konstantin <kostochka1994@mail.ru>
2022-07-07 19:52:56 +0300

    Develop: разбираюсь с каталогами и деревом

--- main.cpp
+++ main.cpp
@@ -16,31 +16,32 @@ int main(int argc, char *argv[])
     //Удаляю пустой аргумент
     strList.removeAt(FRSTARG);
     if(strList.count() > 1) {
+
         for(int i = 1; i < strList.count(); i++) {
             strList[0] += (" " + strList[i]);
         }
-    }
-    if(strList.at(FRSTARG) == HELP) {
-        qInfo() << "В качестве аргумента введите полный путь до папки.";
-    } else {
 
-        QDirIterator itDir(strList.at(FRSTARG), QDir::Dirs);
-        while (itDir.hasNext()) {
-            itDir.next();
-            if(itDir.fileName() == "." || itDir.fileName() == "..") {
-                continue;
+        if(strList.at(FRSTARG) == HELP) {
+            qInfo() << "В качестве аргумента введите полный путь до папки.";
+        } else {
+
+            QDirIterator itDir(strList.at(FRSTARG), QDir::Dirs | QDir::NoDotAndDotDot);
+            while (itDir.hasNext()) {
+                itDir.next();
+                QFileInfo info = itDir.fileInfo();
+                qDebug() << "Папка:" << info;
+
             }
-            QFileInfo info = itDir.fileInfo();
-            qDebug() << "Папка:" << info;
 
+            QDirIterator itFile(strList.at(FRSTARG), QDir::Files);
+            while (itFile.hasNext()) {
+                itFile.next();
+                QFileInfo info = itFile.fileInfo();
+                qDebug() << "Файл:" << info;
+            }
         }
-
-        QDirIterator itFile(strList.at(FRSTARG), QDir::Files);
-        while (itFile.hasNext()) {
-            itFile.next();
-            QFileInfo info = itFile.fileInfo();
-            qDebug() << "Файл:" << info;
-        }
+    } else {
+        qInfo() << "Не были введены аргументы";
     }
 
     return a.exec();
