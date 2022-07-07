549af08e7594784648e4a0c667c7a8c1a9968c82
Konstantin <kostochka1994@mail.ru>
2022-07-07 19:45:16 +0300

    Develop: разбираюсь с каталогами и деревом

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDir>
+#include <QDirIterator>
+#include <QFileInfo>
+
+#define HELP        "--help"
+#define FRSTARG     0
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    qInfo() << "Для справки введите в качестве аргемента: ''--help''";
+    //Записываю аргументы в стринг лист
+    QStringList strList = a.arguments();
+    //Удаляю пустой аргумент
+    strList.removeAt(FRSTARG);
+    if(strList.count() > 1) {
+        for(int i = 1; i < strList.count(); i++) {
+            strList[0] += (" " + strList[i]);
+        }
+    }
+    if(strList.at(FRSTARG) == HELP) {
+        qInfo() << "В качестве аргумента введите полный путь до папки.";
+    } else {
+
+        QDirIterator itDir(strList.at(FRSTARG), QDir::Dirs);
+        while (itDir.hasNext()) {
+            itDir.next();
+            if(itDir.fileName() == "." || itDir.fileName() == "..") {
+                continue;
+            }
+            QFileInfo info = itDir.fileInfo();
+            qDebug() << "Папка:" << info;
+
+        }
+
+        QDirIterator itFile(strList.at(FRSTARG), QDir::Files);
+        while (itFile.hasNext()) {
+            itFile.next();
+            QFileInfo info = itFile.fileInfo();
+            qDebug() << "Файл:" << info;
+        }
+    }
+
+    return a.exec();
+}
