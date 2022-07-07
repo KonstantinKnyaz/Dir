82e8e4c1988eb1a55ba0effadcbaa4b10c87b5a0
Konstantin <kostochka1994@mail.ru>
2022-07-07 21:29:08 +0300

    rec: рекурсия

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #define HELP        "--help"
 #define FRSTARG     0
 
+void dirInfo(const QString &dirPath);
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -15,7 +17,7 @@ int main(int argc, char *argv[])
     QStringList strList = a.arguments();
     //Удаляю пустой аргумент
     strList.removeAt(FRSTARG);
-    if(strList.count() > 1) {
+    if(strList.count() > 0) {
 
         for(int i = 1; i < strList.count(); i++) {
             strList[0] += (" " + strList[i]);
@@ -24,25 +26,30 @@ int main(int argc, char *argv[])
         if(strList.at(FRSTARG) == HELP) {
             qInfo() << "В качестве аргумента введите полный путь до папки.";
         } else {
-
-            QDirIterator itDir(strList.at(FRSTARG), QDir::Dirs | QDir::NoDotAndDotDot);
-            while (itDir.hasNext()) {
-                itDir.next();
-                QFileInfo info = itDir.fileInfo();
-                qDebug() << "Папка:" << info;
-
-            }
-
-            QDirIterator itFile(strList.at(FRSTARG), QDir::Files);
-            while (itFile.hasNext()) {
-                itFile.next();
-                QFileInfo info = itFile.fileInfo();
-                qDebug() << "Файл:" << info;
-            }
+            dirInfo(strList.at(FRSTARG));
         }
     } else {
         qInfo() << "Не были введены аргументы";
     }
 
     return a.exec();
+}
+
+// ---Рекурсия---
+void dirInfo(const QString &dirPath) {
+
+    QDirIterator itDir(dirPath, QDir::Dirs | QDir::NoDotAndDotDot);
+    while (itDir.hasNext()) {
+        itDir.next();
+        QFileInfo info = itDir.fileInfo();
+        qDebug() << "Папка:" << info;
+        dirInfo(itDir.filePath());
+    }
+
+    QDirIterator itFile(dirPath, QDir::Files);
+    while (itFile.hasNext()) {
+        itFile.next();
+        QFileInfo info = itFile.fileInfo();
+        qDebug() << "Файл:" << info;
+    }
 }
